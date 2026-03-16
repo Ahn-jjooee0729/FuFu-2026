2f299ff045e7719eaef05c7ccfe744438ecf2c03
Hyojeong Ahn <jjooee0729@gmail.com>
2026-03-17 02:02:22 +0900

    move firebase config to env

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDajCGPgMf57twSUYP9qazQQuAiLDAUaTw",
-  authDomain: "footfall-20261.firebaseapp.com",
-  projectId: "footfall-20261",
-  storageBucket: "footfall-20261.firebasestorage.app",
-  messagingSenderId: "735575199895",
-  appId: "1:735575199895:web:4db441f2ef44009455d9d2",
-  measurementId: "G-M6JP26J12L"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
